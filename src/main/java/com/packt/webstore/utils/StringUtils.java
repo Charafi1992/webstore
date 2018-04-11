b839a4d75fb5c8712399b6c729e63f4c72c7f7ce
CHARAFI Saad <mrsaad.charafi@gmail.com>
2018-04-11 10:08:45 +0100

    Comit StirngUtils

--- src/main/java/com/packt/webstore/utils/StringUtils.java
+++ src/main/java/com/packt/webstore/utils/StringUtils.java
@@ -1,5 +1,19 @@
 package com.packt.webstore.utils;
 
 public class StringUtils {
-
+	private String name;
+	private String function;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFunction() {
+		return function;
+	}
+	public void setFunction(String function) {
+		this.function = function;
+	}
+	
 }
